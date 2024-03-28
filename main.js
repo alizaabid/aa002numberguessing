e67b855192fa60453ad29c7e5ea07f0b6921812b
alizaabid <155987307+alizaabid@users.noreply.github.com>
2024-03-28 20:11:33 +0500

    push on git

---
+++ main.js
@@ -0,0 +1,14 @@
+#! /urs/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([{
+        name: "userGuessedNumber",
+        type: "number",
+        message: "pleased guessed a number:",
+    },]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("congratulations! you guessed right number.");
+}
+else {
+    console.log("you guessed a wrong number");
+}
